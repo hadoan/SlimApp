4107d5cdc982373f51c634d51078aca65bb41cd8
Ha Doan <ha.doanmanh@gmail.com>
2021-07-09 22:00:34 +0000

    work on dbcontext

---
+++ SlimApp.Core/Events/Bus/Entities/EntityChangeEntry.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Abp.Events.Bus.Entities
+{
+
+    [Serializable]
+    public class EntityChangeEntry
+    {
+        public object Entity { get; set; }
+
+        public EntityChangeType ChangeType { get; set; }
+
+        public EntityChangeEntry(object entity, EntityChangeType changeType)
+        {
+            Entity = entity;
+            ChangeType = changeType;
+        }
+    }
+}
